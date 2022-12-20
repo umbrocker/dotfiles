05d7498d568a1123a0bfd42a3471607278296af5
Gipsz Jakab <gipsz@jakab.com>
2022-12-20 15:28:30 +0100

    Modified installer script.

--- install.sh
+++ install.sh
@@ -2,26 +2,28 @@
 
 g="\e[1;32m"
 reset="\e[0m"
+r="\e[1;31m"
 
-echo "$g[+] Installing packages."
-echo "[*] Need sudo permissions.$reset"
+echo -e "$g[+] Installing packages."
+echo -e "$r[*] Need sudo permissions.$reset"
 sudo apt update
 sudo apt install i3 i3blocks rofi feh lxappearance compton -y
-echo "$g[*] Packages installed."
-echo "[+] Creating dirs.$reset"
+echo -e "$g[*] Packages installed."
+echo -e "[+] Creating dirs.$reset"
 mkdir -p $HOME/.config
 mkdir -p $HOME/.fonts
 mkdir -p $HOME/Képek
-echo "$g[*] Dirs created."
-echo "[+] Copying configs, fonts and wallpapers.$reset"
+echo -e "$g[*] Dirs created."
+echo -e "[+] Copying configs, fonts and wallpapers.$reset"
 sed -i "s/kali/$USER/g" ./config/i3/config
+sed -i "s/kali/$USER/g" ./config/i3/wpc.sh
 cp ./fonts/*.ttf $HOME/.fonts/
 cp -R ./config/* $HOME/.config/
 cp .vimrc $HOME/
 cp .gtkrc-2.0 $HOME/ 
 cp -R ./Wallpapers $HOME/Képek/
-echo "$g[*] Configs and fonts copied."
-echo "[+] Downloading and installing themes.$reset"
+echo -e "$g[*] Configs and fonts copied."
+echo -e "[+] Downloading and installing themes.$reset"
 git clone https://github.com/umbrocker/Nordic.git
 git clone https://github.com/umbrocker/Nordzy-icon.git
 sudo mv Nordic /usr/share/themes
@@ -29,5 +31,5 @@ cd Nordzy-icon
 chmod +x install.sh
 ./install.sh
 cd ..
-echo "$g[*] Themes downloaded and installed."
-echo "[*] Installation completed.$reset"
+echo -e "$g[*] Themes downloaded and installed."
+echo -e "[*] Installation completed.$reset"
