5101a7a28573f043a9d9f95e667e45bcea285e16
Gipsz Jakab <gipsz@jakab.com>
2022-12-20 11:26:42 +0100

    Changes on install.sh

--- install.sh
+++ install.sh
@@ -12,11 +12,13 @@ echo "[+] Creating dirs."
 mkdir -p $home/.config/i3
 mkdir -p $home/.fonts
 echo "[*] Dirs created."
-echo "[+] Copying configs and fonts."
+echo "[+] Copying configs, fonts and wallpapers."
+sed -i "s/kali/$user" ./config/i3/config
 cp ./fonts/*.ttf $home/.fonts/
 cp -R ./config/* $home/.config/
 cp .vimrc $home/
 cp .gtkrc-2.0 $home/ 
+cp -R ./Wallpapers $home/Képek/
 echo "[*] Configs and fonts copied."
 echo "[+] Downloading and installing themes."
 git clone https://github.com/EliverLara/Nordic.git
